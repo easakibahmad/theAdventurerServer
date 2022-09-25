41708f7c17b31c427da9e787fb13bf1a30841dfa
easakibahmad <sakib.sgc.du@gmail.com>
2022-09-25 21:59:43 +0600

    identified clicked items with two funtion

--- module-47-5/src/components/ArrayObjApply/Apply.js
+++ module-47-5/src/components/ArrayObjApply/Apply.js
@@ -1,15 +1,22 @@
 import React, { useState } from 'react';
 import './Apply.css'
 const Apply = (props) => {
-    const {company, phone}=props.index
+    const {company, phone,_id}=props.index
     const [one, oneFunction]=useState(0)
-    const addValue = ()=>oneFunction(one+1)
+    const addValue = (id)=>{
+        // console.log('clicked')
+        oneFunction(one+1)}
+
+    const addValueTwo = ()=>{
+        addValue(_id)
+        console.log('clicked  '+_id)
+    }
     return (
         <div className='applydiv'>
           <h1>name: {company}</h1>
           <h2>salary: {phone}</h2>
           <h4>Clock: {one}</h4>
-          <button onClick={addValue}>Click to Plus</button>
+          <button onClick={addValueTwo}>Click to Plus</button>
         </div>
     );
 };
