5311c1cf32129e3976042e247c84c034a00cd044
easakibahmad <sakib.sgc.du@gmail.com>
2022-09-25 21:49:10 +0600

    button and state changing added

--- module-47-5/src/components/ArrayObjApply/Apply.js
+++ module-47-5/src/components/ArrayObjApply/Apply.js
@@ -1,11 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Apply.css'
 const Apply = (props) => {
     const {company, phone}=props.index
+    const [one, oneFunction]=useState(0)
+    const addValue = ()=>oneFunction(one+1)
     return (
         <div className='applydiv'>
           <h1>name: {company}</h1>
           <h2>salary: {phone}</h2>
+          <h4>Clock: {one}</h4>
+          <button onClick={addValue}>Click to Plus</button>
         </div>
     );
 };
