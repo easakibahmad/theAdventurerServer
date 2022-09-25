6c2cf974ede70e512229584f23e13b4418b35154
easakibahmad <sakib.sgc.du@gmail.com>
2022-09-25 20:16:55 +0600

    export two function

--- module-47-5/src/App.js
+++ module-47-5/src/App.js
@@ -1,13 +1,18 @@
 import './App.css';
 import Sakib from './components/Sakib/Sakib';
 import addNum from './components/Sani.js/Sani';
+import { one,two } from './components/TwoFunction/TwoFunction';
 
 function App() {
   const sum = addNum(23,25)
+  const sumOne= one(12,45)
+  const multiply = two(34,67)
   return (
     <div className="App">
       <Sakib></Sakib>
       <h2>Sum: {sum}</h2>
+      <h3>Sumone: {sumOne}</h3>
+      <h3>Multiply: {multiply}</h3>
     </div>
   );
 }
---
+++ module-47-5/src/components/TwoFunction/TwoFunction.js
@@ -0,0 +1,7 @@
+function one(a,b){
+    return a+b
+}
+function two(a,b){
+    return a*b
+}
+export {one,two}
