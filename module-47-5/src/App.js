cfb8cc2dda0685f88969ec432137c4fa1968844b
easakibahmad <sakib.sgc.du@gmail.com>
2022-09-25 20:12:21 +0600

    export default function added

--- module-47-5/src/App.js
+++ module-47-5/src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
 import Sakib from './components/Sakib/Sakib';
+import addNum from './components/Sani.js/Sani';
 
 function App() {
+  const sum = addNum(23,25)
   return (
     <div className="App">
       <Sakib></Sakib>
+      <h2>Sum: {sum}</h2>
     </div>
   );
 }
