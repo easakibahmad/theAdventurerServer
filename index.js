5f58c8f0039f8e951ea3bc5035e25502ad50aed3
easakibahmad <sakib.sgc.du@gmail.com>
2022-11-08 15:49:57 +0600

    service adding api created

---
+++ index.js
@@ -0,0 +1,53 @@
+const express = require("express");
+const app = express();
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const cors = require("cors");
+const port = process.env.PORT || 5000;
+require("dotenv").config();
+
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@firstcluster.3g9daa6.mongodb.net/?retryWrites=true&w=majority`;
+// console.log(uri);
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+  try {
+    const placeCollection = client.db("theAdventurer").collection("services");
+
+    app.get("/services", async (req, res) => {
+      const query = {};
+      const cursor = placeCollection.find(query);
+      const places = await cursor.limit(3).toArray();
+      res.send(places);
+    });
+
+    app.get("/allservices", async (req, res) => {
+      const query = {};
+      const cursor = placeCollection.find(query);
+      const places = await cursor.toArray();
+      res.send(places);
+    });
+
+    app.post("/service", async (req, res) => {
+      const order = req.body;
+      const result = await placeCollection.insertOne(order);
+      res.send(result);
+    });
+  } finally {
+  }
+}
+run().catch((err) => console.log(err));
+
+app.get("/", (req, res) => {
+  res.send("The Adventurer server is running");
+});
+
+app.listen(port, () => {
+  console.log(`server is running at ${port}`);
+});
